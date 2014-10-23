207147e72bdaea4f8a43b55d3e928f767b62be74
klaoha06 <laohasongkram.supakorn@gmail.com>
2014-10-22 18:30:15 -0700

    daf

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,21 +3,25 @@ get '/' do
 end
 
 get '/log_in' do
+	p APP_ID
+	p "--" * 10
   redirect to("https://www.facebook.com/dialog/oauth?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&scope=user_friends")
 end
 
 get '/auth' do
-  if params[:code]
-   	get_sweet_access_token(params[:code])
-    # erb :test
-  else
-    redirect '/'
-  end
+	p params
+	p params[:code]
+  # if params[:code]
+  #  	get_sweet_access_token(params[:code])
+  #   # erb :test
+  # else
+  #   redirect '/'
+  # end
 end
 
-def get_sweet_access_token(code)
-  response = HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&client_secret=#{APP_SECRET}&code=#{code}")
-  # p response[:expires]
-  # p response
-  p response[:access_token]
-end
+# def get_sweet_access_token(code)
+#   response = HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&client_secret=#{APP_SECRET}&code=#{code}")
+#   # p response[:expires]
+#   # p response
+#   p response[:access_token]
+# end
