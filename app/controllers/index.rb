e326583eba40ff702272e366d6e75772113b9e90
klaoha06 <laohasongkram.supakorn@gmail.com>
2014-10-22 17:16:22 -0700

    test out FB api

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,20 @@
 get '/' do
-	erb :index
+  erb :index
+end
+
+get '/log_in' do
+  redirect to("https://www.facebook.com/dialog/oauth?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}")
+end
+
+get '/auth' do
+  if params[:code]
+    p get_sweet_access_token(params[:code])
+  else
+    p "got some params or something"
+    p params
+  end
+end
+
+def get_sweet_access_token(code)
+  p HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&client_secret=#{APP_SECRET}&code=#{code}")
 end
--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,11 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+APP_ID = ENV['APP_ID']
+APP_SECRET = ENV['APP_SECRET']
+REDIRECT_URI = "https://desolate-wave-5541.herokuapp.com/auth"
+
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
