174c5ea9446a763afefea5af33de8a05178498b8
klaoha06 <laohasongkram.supakorn@gmail.com>
2014-10-22 17:25:18 -0700

    fixing route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,14 +7,15 @@ get '/log_in' do
 end
 
 get '/auth' do
-  if params[:code]
-    p get_sweet_access_token(params[:code])
-  else
-    p "got some params or something"
-    p params
-  end
+	p params
+  # if params[:code]
+  #   p get_sweet_access_token(params[:code])
+  # else
+  #   p "got some params or something"
+  #   p params
+  # end
 end
 
 def get_sweet_access_token(code)
-  p HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&client_secret=#{APP_SECRET}&code=#{code}")
+  # p HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{APP_ID}&redirect_uri=#{REDIRECT_URI}&client_secret=#{APP_SECRET}&code=#{code}")
 end
